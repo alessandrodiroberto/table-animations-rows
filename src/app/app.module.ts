77040b78557e4bff557df1eac8c220b4122a8dd1
alessandrodiroberto <alessandro.diroberto@intecs.it>
2024-02-13 11:26:17 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MatNativeDateModule} from "@angular/material/core";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    MatNativeDateModule,
-    MatDatepickerModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatTableModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
